02e60c42626f2bb8fae49b3e5f34b03989d33ce2
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2024-05-27 23:05:11 +0200

    Add Two Numbers and Word Break Two
    https://leetcode.com/problems/add-two-numbers/description/
    https://leetcode.com/problems/word-break-ii/description/?envType=daily-question&envId=2024-05-25

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode node1 = new ListNode(2);
+            ListNode node2 = new ListNode(4);
+            ListNode node3 = new ListNode(9);
+
+            ListNode node4 = new ListNode(5);
+            ListNode node5 = new ListNode(6);
+            ListNode node6 = new ListNode(4);
+            ListNode node7 = new ListNode(9);
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = null;
+
+            node4.next = node5;
+            node5.next = node6;
+            node6.next = node7;
+            node7.next = null;
+            /*ListNode node1 = new ListNode(0);
+            ListNode node2 = new ListNode(0);
+
+            node1.next = null;
+            node2.next = null;*/
+
+            PrintAllNodesInList(AddTwoNumbers(node1, node4));
+            Console.ReadLine();
+        }
+
+        public static void PrintAllNodesInList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.val);
+                head = head.next;
+            }
+        }
+
+        /*public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }*/
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/add-two-numbers/
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+            int prevExcess = 0, sum = 0;
+            ListNode baseNode = null, currentNode = null;
+
+            for (; l1 != null || l2 != null;)
+            {
+                if (l1 == null)
+                {
+                    sum = l2.val + prevExcess;
+                }
+                else
+                {
+                    if (l2 == null)
+                    {
+                        sum = l1.val + prevExcess;
+                    }
+                    else
+                    {
+                        sum = l1.val + l2.val + prevExcess;                      
+                    }
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                    l2 = l2.next;
+
+                if (baseNode == null)
+                {
+                    baseNode = new ListNode(sum % 10);
+                    currentNode = baseNode;
+                }
+                else
+                {
+                    currentNode.next = new ListNode(sum % 10);
+                    currentNode = currentNode.next;
+                }
+                prevExcess = sum / 10;
+            }
+
+            if (prevExcess == 1)
+                currentNode.next = new ListNode(1);           
+
+            return baseNode;
+        }
+    }
+}
---
+++ WordBreakTwo/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordBreakTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aaaaaaa";
+
+            List<string> wordDict = new List<string>
+            {
+                "aaaa", "aaa"
+            };
+
+            int[] hashDict = GetHashDictionaryArr(wordDict);
+            IList<string> list = WordBreak(s, wordDict);
+           // Console.WriteLine(CreateListOfWords(s, hashDict, 0, "", wordDict, ref varList, 0).ToString());
+            PrintAllLexemesFromList(list);
+            Console.ReadLine();
+        }
+
+        static void PrintAllLexemesFromList(IList<string> varList)
+        {
+            foreach(string variation in varList)
+            {
+                Console.WriteLine(variation);
+            }
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/word-break-ii/?envType=daily-question&envId=2024-05-25
+        /// </summary>
+        /// <param name="sentance"></param>
+        /// <param name="hashDictionary"></param>
+        /// <param name="indexOfLexemeInSentance">index of element from with the analysis of a new lexeme is started</param>
+        /// <param name="lexemes"></param>
+        /// <param name="wordDict"></param>
+        /// <param name="varList"></param>
+        /// <returns>false if there are no alternative lexemes</returns>
+        public static bool CreateListOfWords(string sentance, int[] hashDictionary, int indexOfLexemeInSentance, string lexemes,
+            IList<string> wordDict, ref List<string> varList)
+        {
+            int[] alternativeLexemesIndArr = CreateArrayOfAlternativeLexemeInd(indexOfLexemeInSentance, sentance, wordDict, hashDictionary);
+            if (alternativeLexemesIndArr == null && indexOfLexemeInSentance < sentance.Length) //rest of the sentance is not a valid lexeme in the dictionary       
+                return false;
+            if (alternativeLexemesIndArr == null && lexemes.Length != 0 && indexOfLexemeInSentance == sentance.Length)
+            {
+                varList.Add(lexemes);
+                return true;
+            }
+
+            int count = 0;
+            foreach (int lexemeInd in alternativeLexemesIndArr)
+            {
+                if (alternativeLexemesIndArr.Length > 1 && lexemes.Length != 0 && count != 0)
+                {
+                    if (lexemes.Contains(" "))
+                        lexemes = lexemes.Remove(lexemes.LastIndexOf(" "));
+                    else
+                        lexemes = lexemes.Remove(0);
+                }
+
+                if (lexemes.Length != 0)
+                    lexemes = lexemes + " ";
+                lexemes += wordDict[lexemeInd];
+                bool result = CreateListOfWords(sentance, hashDictionary, indexOfLexemeInSentance + wordDict[lexemeInd].Length, lexemes,
+                    wordDict, ref varList);
+                count++;
+            }
+
+            if (varList.Count != 0)
+                return true;
+            else
+                return false;
+        }
+        public static IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            List<string> varList = new List<string>();
+            int[] hashDict = GetHashDictionaryArr(wordDict);
+
+            CreateListOfWords(s, hashDict, 0, "", wordDict, ref varList);
+            return varList;
+        }
+        public static int[] CreateArrayOfAlternativeLexemeInd(int indOfNewLexeme, string sentance, IList<string> wordDict, int[] hashDictionary)
+        {
+            int[] lexmeIndArr = new int[wordDict.Count];
+            int counter = 0;
+            /*for (int i = indOfNewLexeme; i < hashDictionary.Length; i++)
+                if (sentance.Substring(indOfNewLexeme, wordDict[i].Length).GetHashCode() == hashDictionary[i])
+                {
+                    lexmeIndArr[counter++] = hashDictionary[i];
+                }*/
+
+            for (int i = 0; i < hashDictionary.Length; i++)
+            {
+                if (sentance.Length - wordDict[i].Length - indOfNewLexeme >= 0)
+                    if (sentance.Substring(indOfNewLexeme, wordDict[i].Length).GetHashCode() == hashDictionary[i])
+                    {
+                        lexmeIndArr[counter++] = i;
+                    }
+            }
+
+            if (counter == 0) 
+                return null;
+
+            int[] result = new int[counter];
+            Array.Copy(lexmeIndArr, result, counter);
+            return result;
+        }
+        public static int[] GetHashDictionaryArr(IList<string> wordDict)
+        {
+            int[] hashArr = new int[wordDict.Count];
+
+            for(int i = 0; i < wordDict.Count; i++)
+            {
+                hashArr[i] = wordDict[i].GetHashCode();
+            }
+            return hashArr;
+        }
+    }
+}
